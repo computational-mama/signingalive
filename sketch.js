0ba67b987f01671ca3f221e974c6a83cba31cb8c
ambikajo <ambikajoshi@gmail.com>
2022-02-01 23:12:43 +0530

    color options

--- sketch.js
+++ sketch.js
@@ -1,18 +1,31 @@
 let ltr
-
+let transparentBtn
+let yellowBtn
+let grayBtn
+let islImg
 function setup() {
   noCanvas()
-  
+  radio = createRadio();
+  radio.parent("#options")
+  radio.option('transparent');
+  radio.option('yellow');
+  radio.option('gray');
+
 }
 
-function draw() {
-  background(220);
+function mySelectEvent() {
+  // transparentBtn.mousePressed(()=>{islImg = "assets/transparent/"+ltr+".png"})
+  // yellowBtn.mousePressed(()=>{islImg = "assets/yellow/ISLYellow-"+ltr+".jpg"})
+  // grayBtn.mousePressed(()=>{islImg = "assets/gray/ISLGrey-"+ltr+".jpg"})
+ 
 }
+
 
 function keyTyped(){
   console.log(key)
   ltr = key
-  
+  islImg = "assets/transparent/"+ltr+".png"
+
   if(key === " "){
     makeSpace = createImg("assets/space.png")
     makeSpace.addClass("words")
@@ -22,17 +35,29 @@ function keyTyped(){
     lineBreak.addClass("words")
     lineBreak.parent("#container")
   } else {
-    makeWords = createImg("assets/"+ltr+".png")
+    if(radio.value() === "gray"){
+      islImg = "assets/gray/ISLGrey-"+ltr+".jpg"
+    } else if(radio.value() === "yellow") {
+      islImg = "assets/yellow/ISLYellow-"+ltr+".jpg"
+    } else {
+      islImg = "assets/transparent/"+ltr+".png"
+
+    }
+    makeWords = createImg(islImg)
     makeWords.addClass("words")
     makeWords.parent("#container")
 
   }
-  
+
+
 }
 
 function keyPressed(){
   if(key == "Backspace"){
     words = selectAll(".words")
-   words[words.length-1].remove()
+    if(words.length>0){
+      
+      words[words.length-1].remove()
+    }
 }
 }
